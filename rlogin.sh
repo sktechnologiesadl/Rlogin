03caad4dd5d4131b95eddc41f25bd72d9bcc2dcf
sktechnologiesadl <sktechnologiesadl@gmail.com>
2022-10-18 07:30:16 +0000

    rlogin file

---
+++ rlogin.sh
@@ -0,0 +1,32 @@
+#!/bin/bash 
+# Rlogin enable 
+echo "##########################"
+echo "Welcome To SKTechnologies"
+echo "##########################"
+
+#System update 
+#yum update -y 
+#sleep 3 
+
+#Install the git 
+#yum install git -y 
+ 
+# Rlogin Enable 
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/'  /etc/ssh/sshd_config
+
+#Restart the sshd service 
+systemctl restart sshd 
+
+#Reset the user password
+user=root
+password=sktech123
+
+#passwd $user 
+echo $password |passwd $user --stdin
+
+echo "Password has been successfully changed"
+
+echo "##############################"
+echo "Succesfully Enabled the RLogin"
+echo "##############################"
+
